719c13a7f0cd80ae7543a931b7e1788aa0e657ea
ACER ASPIRE <sibandesifiso01@gmail.com>
2023-05-30 21:21:38 +0200

    first commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,22 @@
 import handlebars from 'vite-plugin-handlebars';
-
+import { defineConfig } from 'vite';
 import data from './data.json';
-export default {
-    plugins: [handlebars({
-      context : 
 
-          data   
-      
-    })],
-  };
+export default defineConfig({
+  base : '/responsive_webpages/',
+  build: {
+    rollupOptions: {
+        input : {
+            main: 'index.html',
+            bootstrap: 'index-bootstrap.html',
+            materialize: 'index-materialize.html',
+        }
+    },
+  },
+  plugins: [handlebars({
+    context : {
+
+        data   
+    }
+  })],
+});
